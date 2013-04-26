bb6e2b02b47e2b3464d8cdfdf80003969a889c24
Eduard Carreras <ecarreras@gisce.net>
2013-04-26 11:20:27 +0200

    New API for meters implemented

--- effipy/usagepoints.py
+++ effipy/usagepoints.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
+from libsaas.services import base
+
 from . import resource
+from . import meters
 
 
 class UsagePoints(resource.EffiPeopleResource):
     path = 'usagepoints'
+
+    @base.resource(meters.Meters)
+    def meter(self, object_id):
+        return meters.Meters(self, object_id)
+
+    @base.resource(meters.Meters)
+    def meters(self):
+        return meters.Meters(self)
