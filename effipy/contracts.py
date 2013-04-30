024b3fc9a428f681b404978da944827cfc488f18
Eduard Carreras <ecarreras@gmail.com>
2013-04-30 16:15:16 +0200

    Implementing versions of contracts

--- effipy/contracts.py
+++ effipy/contracts.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
+from libsaas.services import base
 
 from . import resource
+
+
+class ContractsVersions(resource.EffiPeopleResource):
+    """Permite gestionar las versions de los contratos.
+    """
+    path = 'versions'
 
 
 class Contracts(resource.EffiPeopleResource):
     """Permite gestionar los contratos de los clientes de la compañía.
     """
     path = 'contracts'
+
+    @base.resource(ContractsVersions)
+    def version(self, object_id):
+        return ContractsVersions(self, object_id)
+
+    @base.resource(ContractsVersions)
+    def versions(self):
+        return ContractsVersions(self)
